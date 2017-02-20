adfc850ba919fdceb98b768ded3dee2317b6f7a8
Bartolomeo Lombardi <bartolomeo@MacBook-Pro-di-sviluppoSanps.local>
2017-02-20 17:34:17 +0100

    Cambiamenti

--- essay/code/inputflags.py
+++ essay/code/inputflags.py
@@ -4,7 +4,6 @@ tf.app.flags.DEFINE_integer("task_index", 0,
 			"Index of task within the job")
 FLAGS = tf.app.flags.FLAGS
 
-# start a server for a specific task
 server = tf.train.Server(cluster, 
                           job_name=FLAGS.job_name,
                           task_index=FLAGS.task_index)
